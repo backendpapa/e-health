4a7f9e9fb7c751becfc99ae9ce1a609eef8344cb
Akeju Paul <akejupaul10@gmail.com>
2021-09-29 14:46:19 +0100

    make it better

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,18 +6,18 @@
     <v-row class="" align="center"   justify="center" no-gutters>
       <v-col cols="12" xl="7" lg="7" class="" >
         <v-card elevation="1" class="pa-4 ma-2">
-          <p class="text-h5 font-weight-bold">Server Status</p>
+          <p class="text-h5 font-weight-bold" style=" color:#484C87;">Server Status</p>
           <v-row class="mb-n2" no-gutters>
          <v-col cols="5">
-            <p class="text-caption mt-n2"><v-icon size="15">mdi-server</v-icon> Server-BRAVO-192.167</p>
+            <p class="text-caption mt-n2" style=" color:#484C87;"><v-icon style=" color:#484C87;" size="15">mdi-server</v-icon> Server-BRAVO-192.167</p>
             
          </v-col>
          <v-col cols="1" />
          <v-col cols="6" >
-            <p class="text-caption mt-n2 d-none d-sm-flex"><v-icon size="15">mdi-database-settings-outline</v-icon> Server Account Quote</p>
+            <p style=" color:#484C87;" class="text-caption mt-n2 d-none d-sm-flex"><v-icon size="15" style=" color:#484C87;">mdi-database-settings-outline</v-icon> Server Account Quote</p>
          </v-col>
             </v-row>
-          <v-row no-gutters >
+          <v-row style="color:#484C87" no-gutters >
             <v-col cols="12" xl="5" lg="5">
                 <v-row align="center" justify="start" no-gutters>
                     <v-col class=" pr-2"  cols="6">
@@ -53,9 +53,9 @@
             <v-col cols="12" xl="1" lg="1" />
             <v-col cols="12" xl="6" lg="6" class="">
               
-              <p class="text-caption d-flex d-sm-none mt-3"><v-icon size="15">mdi-database-settings-outline</v-icon> Server Account Quote</p>
+              <p style=" color:#484C87;" class="text-caption d-flex d-sm-none mt-3"><v-icon style=" color:#484C87;" size="15">mdi-database-settings-outline</v-icon> Server Account Quote</p>
               <v-card class="pa-2" flat style="height:30vh;background-color:#F3F5FD;border-radius:15px">
-                  <p class="text-caption font-weight-bold">Available 32.03GB from 120GB</p>
+                  <p class="text-caption font-weight-bold" style=" color:#484C87;">Available 32.03GB from 120GB</p>
                   <p class="text-caption font-weight-bold mb-n1"><span  style="margin-left:19%;color:#383879">18GB</span>
                   <span  style="margin-left:2%;color:#BABED0">2GB</span><span  style="margin-left:5%;color:#6C85E6">84GB</span></p>
                   <div style="height:10vh;position:absolute:top:0;bottom:0" >
@@ -72,13 +72,13 @@
                   </div>
                 
                   <v-row style="position:absolute;bottom:0" no-gutters>
-                    <p style="font-size:10px" class="mt-2"><v-icon size="12" color="#DBE0EE">mdi-square</v-icon> Free Space</p>
+                    <p style="font-size:10px;color:#484C87;" class="mt-2"><v-icon size="12" color="#DBE0EE">mdi-square</v-icon> Free Space</p>
                     <v-spacer></v-spacer>
-                    <p style="font-size:10px" class="mt-2"><v-icon size="12" color="#383879">mdi-square</v-icon> Swap File</p>
+                    <p style="font-size:10px;color:#484C87;" class="mt-2"><v-icon size="12" color="#383879">mdi-square</v-icon> Swap File</p>
                     <v-spacer></v-spacer>
-                    <p style="font-size:10px" class="mt-2"><v-icon size="12" color="#BABED0">mdi-square</v-icon> Cache</p>
+                    <p style="font-size:10px;color:#484C87;" class="mt-2"><v-icon size="12" color="#BABED0">mdi-square</v-icon> Cache</p>
                     <v-spacer></v-spacer>
-                    <p style="font-size:10px" class="mt-2"><v-icon size="12" color="#6C85E6">mdi-square</v-icon> App Files</p>
+                    <p  style="font-size:10px;color:#484C87;" class="mt-2"><v-icon size="12" color="#6C85E6">mdi-square</v-icon> App Files</p>
                   </v-row>
               </v-card>
             </v-col>
@@ -90,10 +90,10 @@
         <v-card style="height:50%"  class="ma-2 pa-4">
           <v-row no-gutters>
             <v-col cols="7">
-              <p class="font-weight-bold">Logged Commits</p>
-              <p class="text-caption mt-n2"><v-icon size="12">mdi-github</v-icon> Github</p>
-              <p style="font-size:10px" class="mt-n2"><v-icon size="12" color="#BDC1D2">mdi-circle</v-icon> Waiting for Review</p>
-               <p style="font-size:10px" class="mt-n3"><v-icon size="12" color="#6C85E6">mdi-circle</v-icon> Accepted</p>
+              <p class="font-weight-bold" style=" color:#484C87;">Logged Commits</p>
+              <p class="text-caption mt-n2" style=" color:#484C87;"><v-icon style=" color:#484C87;" size="12">mdi-github</v-icon> Github</p>
+              <p style="font-size:10px; color:#484C87;"  class="mt-n2"><v-icon  size="12" color="#BDC1D2">mdi-circle</v-icon> Waiting for Review</p>
+               <p style="font-size:10px;color:#484C87;" class="mt-n3"><v-icon size="12" color="#6C85E6">mdi-circle</v-icon> Accepted</p>
             </v-col>
             <v-col cols="5">
                 <div class="">
@@ -104,8 +104,8 @@
     :sections="sections" :total="100"
     :start-angle="0" :auto-adjust-text-size="true"
     @section-click="handleSectionClick">
-    <h1 class="text-h5 font-weight-bold">16</h1>
-    <p style="font0size:10px" class="">Storage server</p>
+    <h1 class="text-h5 font-weight-bold" style="color:#484C87;">16</h1>
+    <p style="font-size:8px;color:#484C87;" class="">Storage server</p>
   </vc-donut>
                 </div>
             </v-col>
@@ -114,10 +114,10 @@
         <v-card style="height:50%"  class="ma-2 pa-4">
           <v-row no-gutters>
             <v-col cols="7">
-              <p class="font-weight-bold mb-n1">Requesting New Services</p>
-              <p class="text-caption mb-n1">Are you looking for a new tool for your project?</p>
+              <p class="font-weight-bold mb-n1" style="color:#484C87;">Requesting New Services</p>
+              <p class="text-caption mb-n1 " style="color:#484C87;">Are you looking for a new tool for your project?</p>
            
-                <a href="" class="text-caption ">Services list</a>
+                <a href="" style="font-size:10px">Services list</a>
               
             </v-col>
             <v-col cols="5">
@@ -135,16 +135,16 @@
         <v-col cols="12" class="" xl="2" lg="3" >
           <v-card  class=" ma-2" style="border-bottom-right-radius:15px;border-bottom-left-radius:15px">
           <div class="pa-3">
-            <p class="text-caption grey--text font-weight-bold"><v-icon size="30">mdi-gitlab</v-icon> Gitlab</p>
-          <p class="font-weight-bold">Gitlab</p>
+            <p class="text-caption grey--text font-weight-bold" ><v-icon style="color:#484C87;" size="30">mdi-gitlab</v-icon> Gitlab</p>
+          <p class="font-weight-bold" style="color:#484C87;">Gitlab</p>
           <p class="grey--text text-caption mb-n2 mt-n4">Version 1.2.4</p>
           </div>
           <v-btn color="#EBEEF7" style="border-bottom-right-radius:15px;border-bottom-left-radius:15px;text-transform:none" block>
             <v-row no-gutters align="center" justify="center">
               <v-icon color="#ADE648" size="10">mdi-circle</v-icon>
-              <p class="text-caption mt-4 ml-2">Running</p>
+              <p class="text-caption mt-4 ml-2" style="color:#484C87;">Running</p>
               <v-spacer></v-spacer>
-              <v-icon size="10">mdi-information-outline</v-icon>
+              <v-icon size="10" style="color:#484C87;">mdi-information-outline</v-icon>
             </v-row>
           </v-btn>
         </v-card>
@@ -153,26 +153,26 @@
         <v-col cols="12" class="" xl="2" lg="3" >
           <v-card  class=" ma-2" style="border-bottom-right-radius:15px;border-bottom-left-radius:15px">
           <div class="pa-3">
-            <p class="text-caption grey--text font-weight-bold"><v-icon size="30">mdi-oci</v-icon> Oci</p>
-          <p class="font-weight-bold">Oracle Cloud </p>
+            <p class="text-caption grey--text font-weight-bold"><v-icon style="color:#484C87;" size="30">mdi-oci</v-icon> Oci</p>
+          <p class="font-weight-bold" style="color:#484C87;">Oracle Cloud </p>
           <p class="grey--text text-caption mb-n2 mt-n4">Version 1.1.4</p>
           </div>
           <v-btn color="#EBEEF7" style="border-bottom-right-radius:15px;border-bottom-left-radius:15px;text-transform:none" block>
             <v-row no-gutters align="center" justify="center">
               <v-icon color="#ADE648" size="10">mdi-circle</v-icon>
-              <p class="text-caption mt-4 ml-2">Running</p>
+              <p class="text-caption mt-4 ml-2" style="color:#484C87;">Running</p>
               <v-spacer></v-spacer>
-              <v-icon size="10">mdi-information-outline</v-icon>
+              <v-icon size="10" style="color:#484C87;">mdi-information-outline</v-icon>
             </v-row>
           </v-btn>
         </v-card>
         </v-col>
 
          <v-col cols="12" class="" xl="2" lg="3" >
-          <v-card  class=" ma-2" style="border-bottom-right-radius:15px;border-bottom-left-radius:15px">
+          <v-card   class=" ma-2" style="border-bottom-right-radius:15px;border-bottom-left-radius:15px">
           <div class="pa-3">
-            <p class="text-caption grey--text font-weight-bold"><v-icon size="30">mdi-kubernetes</v-icon> Kubernetes</p>
-          <p class="font-weight-bold">Kubernetes </p>
+            <p class="text-caption grey--text font-weight-bold"><v-icon color="red" size="30">mdi-kubernetes</v-icon> Kubernetes</p>
+          <p class="font-weight-bold red--text"  >Kubernetes </p>
           <p class="grey--text text-caption mb-n2 mt-n4">Version 1.1.4</p>
           </div>
           <v-btn color="#FBC0C3" style="border-bottom-right-radius:15px;border-bottom-left-radius:15px;text-transform:none" block>
@@ -180,7 +180,7 @@
               <v-icon color="red" size="10">mdi-circle</v-icon>
               <p class="text-caption mt-4 ml-2 red--text">Crashed</p>
               <v-spacer></v-spacer>
-              <v-icon size="10">mdi-information-outline</v-icon>
+              <v-icon size="10" >mdi-information-outline</v-icon>
             </v-row>
           </v-btn>
         </v-card>
@@ -190,16 +190,16 @@
         <v-col cols="12" class="" xl="2" lg="3" >
           <v-card  class=" ma-2" style="border-bottom-right-radius:15px;border-bottom-left-radius:15px">
           <div class="pa-3">
-            <p class="text-caption grey--text font-weight-bold"><v-icon size="30">mdi-apache-kafka</v-icon> Apache-kafka</p>
-          <p class="font-weight-bold">Apache-Kafka </p>
+            <p class="text-caption grey--text font-weight-bold"><v-icon style="color:#484C87;" size="30">mdi-apache-kafka</v-icon> Apache-kafka</p>
+          <p class="font-weight-bold" style="color:#484C87;">Apache-Kafka </p>
           <p class="grey--text text-caption mb-n2 mt-n4">Version 2.1.3</p>
           </div>
           <v-btn color="#EBEEF7" style="border-bottom-right-radius:15px;border-bottom-left-radius:15px;text-transform:none" block>
             <v-row no-gutters align="center" justify="center">
               <v-icon color="#ADE648" size="10">mdi-circle</v-icon>
-              <p class="text-caption mt-4 ml-2">Running</p>
+              <p class="text-caption mt-4 ml-2" style="color:#484C87;">Running</p>
               <v-spacer></v-spacer>
-              <v-icon size="10">mdi-information-outline</v-icon>
+              <v-icon style="color:#484C87;" size="10">mdi-information-outline</v-icon>
             </v-row>
           </v-btn>
         </v-card>
